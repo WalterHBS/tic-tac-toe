5728403e785a1ce659776cedca702978d6b0f41d
hamed <hamed.bakhshi7809@gmail.com>
2024-03-07 21:38:26 +0100

    gameFlow function to determine next marker

--- script.js
+++ script.js
@@ -9,5 +9,25 @@ const MakePlayer = function(player, marker){
 return {player, marker}
 }
 const gameBoard = makeGameBoard()
+const gameFlow = function(){
+    let counterX = 0
+    let counterO = 0
+let value = gameBoard.keys() 
+for(marks in value){
+    if(marks == 'x'){
+        counterX++;
+    }
+    if(marks == 'O'){
+        counterO++;
+    }
+}
+if(counterX == counterO){
+    return 'X'
+}
+else{
+    return 'O'
+}
+}
+
 const playerOne = MakePlayer('Walter', 'X')
 console.log(gameBoard, playerOne)
